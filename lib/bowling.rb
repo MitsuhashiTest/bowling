d8f4464b135a7c8b8d604d87e80b4f07ea19f293
mitsuhashi <cnsh.mitsuhashi@gmail.com>
2017-10-13 04:41:42 +0000

    cycle1 仮実装

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,13 +1,42 @@
 class Bowling
     def initialize
         @score = 0
+        @all_score = 0
+        
+        @speaFlg = false
+        @count = 0
     end
     
     def add_score(obj)
+        
+        @count += 1
         @score += obj
+        
+        if @speaFlg
+            @all_score += obj
+        end
+        
+        if @count==2
+            @all_score += @score
+            
+            if @score == 10
+                @speaFlg = true
+            else
+                @speaFlg = false
+            end
+            
+            @count = 0
+            @score = 0
+        end
+        
     end
     
     def total_score
         @score
     end
+    
+    def calc_score
+        @score = @all_score
+    end
+   
 end
